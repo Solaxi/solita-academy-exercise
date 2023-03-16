19e27b32cc6203e42eaf89d95eb14413b9bc92a6
Solaxi <esko.pylsy@gmail.com>
2023-03-16 15:17:14 +0200

    Added GET for stations by ID

--- src/main/java/us/solax/bikeapp/controller/BikeAppController.java
+++ src/main/java/us/solax/bikeapp/controller/BikeAppController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,5 +31,10 @@ public class BikeAppController {
   @GetMapping("/stations")
   public List<Station> allStations() {
     return stationRepository.findAll();
+  }
+
+  @GetMapping("/stations/{stationId}")
+  public Station getStation(@PathVariable("stationId") int stationId) {
+    return stationRepository.findByStationId(stationId);
   }
 }
